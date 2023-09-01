c8ac05fb85e73d494b55dad5062cf0d23511adde
Akos Stegner <a.stegner@mito.hu>
2023-09-01 09:25:52 +0200

    Refact auto publish functions

--- netlify/functions/auto-publish.ts
+++ netlify/functions/auto-publish.ts
@@ -1,31 +1,33 @@
 import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
-// import { schedule } from '@netlify/functions';
+import { schedule } from '@netlify/functions';
 import { createClient } from '@supabase/supabase-js';
+
+interface ConvertUpsert {
+  id: number
+}
 
 const { SUPABASE_URL, SUPABASE_KEY } = process.env;
 
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// autoPublishHandler
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+const autoPublishHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   const { data } = await supabaseClient
     .from('posts')
     .select('id')
     .lte('scheduled_at', new Date().toISOString())
     .is('published_at', null);
 
-  await supabaseClient.from('posts').upsert(convertUpsertData(data)).select();
+  await supabaseClient.from('posts').upsert(convertUpsertData(data || [])).select();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
   };
 };
 
-// const handler = schedule('@hourly', autoPublishHandler);
+const handler = schedule('@hourly', autoPublishHandler);
 
 export { handler };
 
-function convertUpsertData(data) {
+function convertUpsertData(data: ConvertUpsert[]) {
   return data.map((row) => ({ ...row, published_at: new Date(), is_active: true }));
 }
