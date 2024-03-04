f1af93d3bf87935a1dbba0b0d2dcdfc70784e184
Akos Stegner <a.stegner@mito.hu>
2024-03-04 12:46:49 +0100

    Change u18 standings

--- pages/u18/standings.vue
+++ pages/u18/standings.vue
@@ -12,57 +12,39 @@ const { locale } = useI18n();
       <table>
         <thead>
           <tr>
-            <th>
-              <div>Pos</div>
-            </th>
-            <th>
-              <div>Team name</div>
-            </th>
+            <th>#</th>
+            <th>Team name</th>
             <th>
               <div>Total tournament participation</div>
             </th>
-            <th>
-              <div>Best 3 results</div>
-            </th>
+            <th>Best 3 results</th>
             <th>GD</th>
-            <th>
-              <div>Tournament points</div>
-            </th>
+            <th>Tournament points</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>1.</td>
-            <td>TRE</td>
-            <td>3</td>
-            <td>
-              <div>23.08.19, 23.09.01, 23.12.16.</div>
-            </td>
-            <td>28</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>2.</td>
-            <td>ITA</td>
-            <td>3</td>
-            <td>
-              <div>23.09.01, 23.08.19, 24.01.06.</div>
-            </td>
-            <td>18</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>3.</td>
             <td>RBS</td>
             <td>4</td>
             <td>
               <div>23.08.19, 23.12.16, 24.01.06</div>
             </td>
             <td>17</td>
-            <td>7</td>
+            <td>8</td>
           </tr>
           <tr>
-            <td>4.</td>
+            <td>2.</td>
+            <td>TRE</td>
+            <td>3</td>
+            <td>
+              <div>23.08.19, 23.09.01, 23.12.16.</div>
+            </td>
+            <td>28</td>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td>3.</td>
             <td>POL</td>
             <td>3</td>
             <td>
@@ -72,18 +54,28 @@ const { locale } = useI18n();
             <td>7</td>
           </tr>
           <tr>
-            <td>5.</td>
-            <td>HKN</td>
-            <td>2</td>
+            <td>4.</td>
+            <td>ITA</td>
+            <td>3</td>
             <td>
-              <div>23.08.18, 23.09.01.</div>
+              <div>23.09.01, 23.08.19, 24.01.06.</div>
             </td>
-            <td>20</td>
-            <td>6</td>
+            <td>18</td>
+            <td>7</td>
+          </tr>
+          <tr>
+            <td>5.</td>
+            <td>SLO</td>
+            <td>3</td>
+            <td>
+              <div>23.09.02, 23.09.01, 23.12.16.</div>
+            </td>
+            <td>-6</td>
+            <td>7</td>
           </tr>
           <tr>
             <td>6.</td>
-            <td>SBR</td>
+            <td>SBA</td>
             <td>3</td>
             <td>
               <div>23.08.19, 23.09.02, 23.12.16.</div>
@@ -103,16 +95,26 @@ const { locale } = useI18n();
           </tr>
           <tr>
             <td>8.</td>
-            <td>LJS</td>
-            <td>3</td>
+            <td>NIT</td>
+            <td>2</td>
             <td>
-              <div>23.09.02, 23.09.01, 23.12.16.</div>
+              <div>23.08.18, 23.09.01.</div>
             </td>
-            <td>-6</td>
-            <td>5</td>
+            <td>20</td>
+            <td>6</td>
           </tr>
           <tr>
             <td>9.</td>
+            <td>THU</td>
+            <td>2</td>
+            <td>
+              <div>23.09.02, 23.12.16.</div>
+            </td>
+            <td>-1</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>10.</td>
             <td>SEC</td>
             <td>3</td>
             <td>
@@ -122,18 +124,26 @@ const { locale } = useI18n();
             <td>5</td>
           </tr>
           <tr>
-            <td>10.</td>
-            <td>THU</td>
-            <td>2</td>
+            <td>11.</td>
+            <td>KYI</td>
+            <td>4</td>
             <td>
-              <div>23.09.02, 23.12.16.</div>
+              <div>23.09.01, 23.08.19, 23.12.16.</div>
             </td>
-            <td>-1</td>
+            <td>-7</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>12.</td>
+            <td>ZLN</td>
+            <td>1</td>
+            <td>23.09.01-03.</td>
+            <td>4</td>
             <td>4</td>
           </tr>
           <tr>
-            <td>11.</td>
-            <td>EAS</td>
+            <td>13.</td>
+            <td>GRA</td>
             <td>3</td>
             <td>
               <div>23.09.01, 23.08.19, 24.01.06.</div>
@@ -142,17 +152,7 @@ const { locale } = useI18n();
             <td>4</td>
           </tr>
           <tr>
-            <td>12.</td>
-            <td>KYI</td>
-            <td>4</td>
-            <td>
-              <div>23.09.01, 23.08.19, 23.12.16.</div>
-            </td>
-            <td>-7</td>
-            <td>4</td>
-          </tr>
-          <tr>
-            <td>13.</td>
+            <td>14.</td>
             <td>POR</td>
             <td>2</td>
             <td>
@@ -162,17 +162,15 @@ const { locale } = useI18n();
             <td>3</td>
           </tr>
           <tr>
-            <td>14.</td>
-            <td>ZLN</td>
+            <td>15.</td>
+            <td>PLS</td>
             <td>1</td>
-            <td>
-              <div>23.09.01-03.</div>
-            </td>
-            <td>4</td>
+            <td>23.09.01-03.</td>
+            <td>0</td>
             <td>3</td>
           </tr>
           <tr>
-            <td>15.</td>
+            <td>16.</td>
             <td>FEH</td>
             <td>3</td>
             <td>
@@ -182,7 +180,7 @@ const { locale } = useI18n();
             <td>3</td>
           </tr>
           <tr>
-            <td>16.</td>
+            <td>17.</td>
             <td>MIS</td>
             <td>3</td>
             <td>
@@ -192,7 +190,7 @@ const { locale } = useI18n();
             <td>2</td>
           </tr>
           <tr>
-            <td>17.</td>
+            <td>18.</td>
             <td>BJA</td>
             <td>3</td>
             <td>
@@ -202,43 +200,29 @@ const { locale } = useI18n();
             <td>2</td>
           </tr>
           <tr>
-            <td>18.</td>
+            <td>19.</td>
             <td>KAC</td>
             <td>1</td>
-            <td>
-              <div>23.08.18-20.</div>
-            </td>
+            <td>23.08.18-20.</td>
             <td>-5</td>
             <td>1</td>
           </tr>
           <tr>
-            <td>19.</td>
-            <td>VAS</td>
-            <td>1</td>
-            <td>
-              <div>23.09.01-03.</div>
-            </td>
-            <td>-6</td>
-            <td>0</td>
-          </tr>
-          <tr>
             <td>20.</td>
-            <td>KRA</td>
-            <td>1</td>
-            <td>
-              <div>23.09.01-03.</div>
-            </td>
-            <td>-12</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <td>21.</td>
             <td>VIC</td>
             <td>4</td>
             <td>
               <div>23.08.19, 23.09.02, 24.01.06.</div>
             </td>
             <td>-31</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>21.</td>
+            <td>VSC</td>
+            <td>1</td>
+            <td>23.09.01-03.</td>
+            <td>-6</td>
             <td>0</td>
           </tr>
         </tbody>
