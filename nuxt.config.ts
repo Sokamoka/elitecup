4d433752d4d0fd66d6f2c6df2da2c1dd8167d3e0
Sokamoka <akos.stegner@gmail.com>
2025-08-07 18:03:24 +0200

    Patch GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import tailwindTypography from '@tailwindcss/typography';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-08-07',
+
   ssr: false,
 
   nitro: {
